56ea6243d3a0459ce63b6e112bd80785bf59d377
Aviv Yunker <avivyunker@gmail.com>
2025-11-24 12:13:02 +0200

    complete cleaning procedure of project (complete day 7)

--- weather_pipeline/Silver/silver_script.py
+++ weather_pipeline/Silver/silver_script.py
@@ -1,5 +1,7 @@
 import os
 import json
+import pandas as pd
+from datetime import datetime
 
 
 bronze_path = os.path.join(os.path.dirname(__file__), "..", "bronze")
@@ -59,4 +61,95 @@ except FileNotFoundError:
 except json.JSONDecodeError as e:
     print("JSON error:", e)
 
-print(dict_results)
+# print(dict_results)
+
+# Rename key "city_id" to be "City ID"
+
+# convert the python dictionary to a dataframe
+df = pd.DataFrame([dict_results])  # wrap the dict in a list
+
+# change the name of each of the keys of the dataframe:
+df = df.rename(columns={
+    "city_id": "City ID",
+    "city_name": "City Name",
+    "country": "Country",
+    "coord_lon": "Longitude",
+    "coord_lat": "Latitude",
+    "weather_id": "Weather ID",
+    "weather_main": "Weather Main",
+    "weather_description": "Weather Description",
+    "weather_icon": "Weather Icon",
+    "temp": "Temperature",
+    "feels_like": "Feels Like",
+    "temp_min": "Minimum Temperature",
+    "temp_max": "Maximum Temperature",
+    "pressure": "Pressure",
+    "humidity": "Humidity",
+    "sea_level": "Sea Level",
+    "grnd_level": "Ground Level",
+    "visibility": "Visibility",
+    "wind_speed": "Wind Speed",
+    "wind_deg": "Wind Degrees",
+    "clouds_all": "Cloudiness Percentage",
+    "sunrise": "Sunrise Time",
+    "sunset": "Sunset Time",
+    "data_time": "Time of Data",
+    "timezone": "Timezone",
+    "base": "Base",
+    "cod": "HTTP Status Code"
+})
+
+# Convert the UNIX times to Human Readable times:
+df["Sunrise Time"] = pd.to_datetime(df["Sunrise Time"], unit="s")
+df["Sunset Time"] = pd.to_datetime(df["Sunset Time"], unit="s")
+df["Time of Data"] = pd.to_datetime(df["Time of Data"], unit="s")
+
+# Convert the tempereature to Celcius:
+def to_celsius (temperature):
+    if (temperature > 100):
+        temperature -= 273.15
+    return temperature
+
+temperature_columns = ["Temperature", "Feels Like", "Minimum Temperature", "Maximum Temperature"]
+
+for temp in temperature_columns:
+    df[temp] = df[temp].apply(to_celsius)
+
+
+# Convert the numeric values to :
+float_cols = ["Temperature", "Feels Like", "Minimum Temperature", "Maximum Temperature", "Wind Speed", "Longitude", "Latitude"]
+int_cols = ["Pressure", "Humidity", "Sea Level", "Ground Level", "Visibility", "Wind Degrees", "Cloudiness Percentage"]
+
+df[float_cols] = df[float_cols].apply(pd.to_numeric, errors="coerce")
+df[int_cols] = df[int_cols].apply(pd.to_numeric, errors="coerce").astype("Int64")
+
+
+# dropping columns that are unnecessary for alanysis:
+df = df.drop('Base', axis=1)
+df = df.drop('HTTP Status Code', axis=1)
+df = df.drop('Timezone', axis=1)
+df = df.drop('Weather Icon', axis=1)
+
+# Filling NaN values with "Unknown" strings
+string_columns = ["City Name", "Country", "Weather Main", "Weather Description"]
+numeric_columns = ["Temperature", "Feels Like", "Minimum Temperature", "Maximum Temperature", "Pressure", "Humidity", "Sea Level", "Ground Level", "Visibility", "Wind Speed", "Wind Degrees", "Cloudiness Percentage", "Longitude", "Latitude"]
+
+df[string_columns] = df[string_columns].fillna("Unknown")
+df[numeric_columns] = df[numeric_columns].fillna(0)
+
+# add metadata to the DataFrame:
+df["Pipeline Version"] = "v1.0"
+df["Data Source"] = "OpenWeather API"
+df["Created By"] = "Aviv Yunker"
+df["Load Datetime"] = pd.Timestamp.now()
+
+# save the DataFrame to a new CSV file
+
+second = datetime.now().second
+minute = datetime.now().minute
+hour = datetime.now().hour
+day = datetime.now().day
+month = datetime.now().month
+year = datetime.now().year
+
+df.to_csv(f"SilverCSV_{day}{month}{year}_{hour}{minute}.csv", sep="\t", encoding='utf-8')
